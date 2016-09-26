1a73f5eeaf946432c085ef3599b15430249277a3
Zach <nandapur@usc.edu>
2016-09-26 15:16:53 -0700

    Added java prime program

---
+++ src/itp325/hw3a.java
@@ -0,0 +1,38 @@
+package itp325;
+
+public class hw3a 
+{
+    public static void main(String[] args) 
+    {
+    	int count = 0;
+       for(int i = 1; i <= 1000000; i++)
+       {
+         if (isPrimeNumber(i))
+         {
+        	 if(count == 10001){
+        		 System.out.println(i);
+        	 }
+        	 else{
+        		 count++;
+        	 }
+            
+         }
+       }
+    }
+
+
+    public static boolean isPrimeNumber(int i) {
+        int factors = 0;
+        int j = 1;
+
+        while(j <= i)
+        {
+            if(i % j == 0)
+            {
+                factors++;
+            }
+            j++;
+        }
+        return (factors == 2);
+  }
+}
